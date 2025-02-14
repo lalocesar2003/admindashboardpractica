6e03d91e93dba583f6262dbd38dc53b6533d87dd
lalocesar2003 <ceed1000@gmail.com>
2025-02-14 13:29:39 -0500

    48 minutos

---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import samalogo from "../img/samalogo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+      <Link href="/">
+        <Image src={samalogo} alt="TraversyPress" width={40} />
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-black">BT</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
